90fdf7e88ce1a3889ffafe2e9a43fb29baf07bac
Mario Cabrera <yankeescab@gmail.com>
2023-05-02 17:40:37 -0400

    commented out sql connection as it isnt used yet and causing the app to crash on heroku

--- config.py
+++ config.py
@@ -9,27 +9,27 @@ import json
 
 # Database context
 
-cnx = mysql.connector.connect(
-    user= os.getenv('DB_USER'),
-    password= os.getenv('DB_PW'),
-    host= os.getenv('DB_HOST'),
-    database= os.getenv('DB_NAME'))
+#cnx = mysql.connector.connect(
+#    user= os.getenv('DB_USER'),
+#    password= os.getenv('DB_PW'),
+#    host= os.getenv('DB_HOST'),
+#    database= os.getenv('DB_NAME'))
 
 
 # Test DB connection
 
-def test_connection():
-    try:
-        cnx
-    except mysql.connector.Error as err:
-        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-            print("Username or Password Error")
-        elif err.errno == errorcode.ER_BAD_DB_ERROR:
-            print("Database not found")
-        else:
-            print(err)
-    else: 
-        print ('We have sucessfully connected to the database')
+#def test_connection():
+#    try:
+#        cnx
+#    except mysql.connector.Error as err:
+#        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+#            print("Username or Password Error")
+#        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+#            print("Database not found")
+#        else:
+#            print(err)
+#    else: 
+#        print ('We have sucessfully connected to the database')
 
 
 
