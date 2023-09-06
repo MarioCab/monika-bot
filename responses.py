d17d2c4d499ba7d8400df8bea195867db72b4c88
Mario Cabrera <yankeescab@gmail.com>
2023-09-06 12:42:26 -0400

    added more images

--- responses.py
+++ responses.py
@@ -7,7 +7,17 @@ general_responses = [
     "Please never leave me"
 ]
 
-images = ["https://i0.wp.com/anitrendz.net/news/wp-content/uploads/2023/03/konosuba-explosion-wonderful-world-key-visual-e1679428222309.png",
+images = ["https://steamuserimages-a.akamaihd.net/ugc/1692773483557994720/3B965C677C383728B27C6FC1A78CA883C28EF153/?imw=512&&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false",
           "https://costumesheaven.b-cdn.net/wp-content/uploads/2022/11/anime-attack-on-titan-mikasa-ackerman-shingeki-no-kyojin-wallpaper-preview.jpg",
           "https://image.civitai.com/xG1nkqKTMzGDvpLrqFT7WA/08dafdeb-e171-4e2a-534d-a194c6b48000/width=450/366074.jpeg",
-            "https://thicc-af.mywaifulist.moe/waifus/rem-re-zero-starting-life-in-another-world/VgElkusFOR8ey3c3ivE7q0Hrd1dMtr9VDgLiE419.jpg?class=thumbnail"]
+            "https://thicc-af.mywaifulist.moe/waifus/rem-re-zero-starting-life-in-another-world/VgElkusFOR8ey3c3ivE7q0Hrd1dMtr9VDgLiE419.jpg?class=thumbnail",
+            "https://resizing.flixster.com/iGUQG1h7Zw_la_zWDzXERE4u9lw=/300x300/v2/https://flxt.tmsimg.com/assets/p2222332_e_v9_ab.jpg",
+            "https://costumesheaven.com/wp-content/uploads/2022/05/%E7%9A%84.jpg",
+            "https://i.pinimg.com/736x/67/a0/f4/67a0f41ac75b1522fb8c84223b0a9bde.jpg",
+            "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F6%2F2006%2F06%2F15518__cars_l.jpg&q=60",
+            "https://s4.anilist.co/file/anilistcdn/character/large/b17-IazKGogQwJ1p.png",
+            "https://static.zerochan.net/Uchiha.Sasuke.full.3260069.png,
+            "https://image.civitai.com/xG1nkqKTMzGDvpLrqFT7WA/7a14d035-ea42-4049-8466-837f4751a000/width=1200/7a14d035-ea42-4049-8466-837f4751a000.jpeg",
+            "https://s4.anilist.co/file/anilistcdn/character/large/b7-JdR4betokDjR.jpg",
+            "https://i.pinimg.com/736x/bd/1d/b5/bd1db58ff2706cf959675b2ca74557d3.jpg"
+            ]
